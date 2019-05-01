39ede42a826c55153675b4afd5b6e167c648ff77
Andrew Swellie <andrewswellie@gmail.com>
2019-05-01 14:31:40 -0400

    saving changes

--- app.py
+++ app.py
@@ -10,20 +10,31 @@ from flask_pymongo import PyMongo
 import scrape_mars
 
 
+app = Flask(__name__)
+
+
 @app.route("/")
 def home():
+    # Find one record of data from the mongo database
+    #destination_data = mongo.db.collection.find_one()
 
-    
+    # Return template and data
+    #return render_template("index.html", vacation=destination_data)    
 
 
 @app.route("/scrape")
 def scrape():
+    # Run the scrape function
+    #costa_data = scrape_costa.scrape_info()
 
-   
+    # Update the Mongo database using update and upsert=True
+    #mongo.db.collection.update({}, costa_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
 
+   
 
+       
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -21,4 +21,29 @@ def init_browser():
 def scrape_info():
     browser = init_browser()
 
+def scrape():
+    mars_data = {}
+    mars_data["news_data"] = marsNewsData()
+    mars_data["mars_featured_image"] = marsFeaturedImage()
+    mars_data["mars_weather"] = marsWeather()
+    mars_data["mars_facts_table"] = marsFacts()
+    mars_data["mars_hemispheres"] = marsHemisphereImages()
+    return mars_data
 
+
+
+# Mars News Data
+def marsNewsData():
+    
+# Mars Featured Image
+def marsFeaturedImage():
+    
+# Mars Weather Data
+def marsWeather():
+    
+# Mars Facts
+def marsFacts():
+    
+# Mars Hemisphere Images
+def marsHemisphereImages():    
+    
