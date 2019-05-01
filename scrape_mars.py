6bc289c558233fc0c59a130b3a12dfddb96dd084
Andrew Swellie <andrewswellie@gmail.com>
2019-05-01 13:57:58 -0400

    saving my changes...adding app.py starter file

--- scrape_mars.py
+++ scrape_mars.py
@@ -5,3 +5,20 @@ Created on Wed May  1 10:05:26 2019
 @author: andre
 """
 
+# Dependencies
+from bs4 import BeautifulSoup as bs
+import requests
+import os
+import requests
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+import pandas as pd
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def scrape_info():
+    browser = init_browser()
+
+
