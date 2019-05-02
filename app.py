94c119a1221268b583ae4d63b18d0f04bd8e1721
Andrew Swellie <andrewswellie@gmail.com>
2019-05-02 12:47:29 -0400

    pushing up most recent changes...everything working, still need to format

--- app.py
+++ app.py
@@ -8,33 +8,44 @@ Created on Wed May  1 13:50:01 2019
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+import pymongo
 
 
+
+# create instance of Flask app
 app = Flask(__name__)
 
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
 
+# connect to mongo db and collection
+db = client.scrape_mars
+collection = db.data
+
+# Route to render index.html template and find mongo documents
 @app.route("/")
-def home():
-    # Find one record of data from the mongo database
-    #destination_data = mongo.db.collection.find_one()
+def index():
+    mars_final_data = db.collection.find_one()
 
     # Return template and data
-    #return render_template("index.html", vacation=destination_data)    
+    return render_template("index.html", mars=mars_final_data)
 
 
+
+# Route that will trigger scrape functions
 @app.route("/scrape")
 def scrape():
-    # Run the scrape function
-    #costa_data = scrape_costa.scrape_info()
+   
+   # Run the scrape function
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    #mongo.db.collection.update({}, costa_data, upsert=True)
+    db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
+    
 
-   
-
-       
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -9,15 +9,19 @@ Created on Wed May  1 10:05:26 2019
 from bs4 import BeautifulSoup as bs
 import requests
 import os
-import requests
 from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 import pandas as pd
+import time
+
+
 
 #Setup splinter browser
 def init_browser():
     executable_path = {"executable_path": "chromedriver"}
-    return Browser("chrome", **executable_path, headless=True)
+    return Browser("chrome", **executable_path, headless=False)
+
+
 
 #Create the scrape function that will run all the other functions and return the outputs
 def scrape():
@@ -28,6 +32,8 @@ def scrape():
     mars_data["mars_facts_table"] = marsFacts()
     mars_data["mars_hemispheres"] = marsHemisphereImages()
     return mars_data
+
+   
 
 
 # Scrape Mars News Data
@@ -47,10 +53,12 @@ def marsNewsData():
     news_data["paragraph_text"]= news_p
 
     return news_data
+
+
     
 # Scrape Mars Featured Image
 def marsFeaturedImage():
-    browser = initBrowser()    
+    browser = init_browser()    
     mars_images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(mars_images_url)
     time.sleep(5)
@@ -61,9 +69,9 @@ def marsFeaturedImage():
     featured_image_url = browser.find_by_css('.fancybox-image').first['src']
     time.sleep(5)
     
-    browser.quit()
-
     return featured_image_url
+
+    
     
 # Scrape Mars Weather Data
 def marsWeather():
@@ -71,20 +79,80 @@ def marsWeather():
     response = requests.get(twitter_url)
     time.sleep(5)
 
-    soup = bs(response.text, 'html.parser')
+    soup2 = bs(response.text, 'html.parser')
     time.sleep(5)
     
     mars_weather = []
-    for weather in soup.find_all('p',class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"):
+    for weather in soup2.find_all('p',class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"):
         mars_weather.append(weather.text.strip())
 
     weather_tweet = mars_weather[0]
 
     return weather_tweet
+
+
     
 # Scrape Mars Facts
 def marsFacts():
+    mars_facts_url = 'https://space-facts.com/mars/'
+       
+    mars_facts = pd.read_html(mars_facts_url)
+    time.sleep(5)
+    
+    mars_facts_df = mars_facts[0]
+    mars_facts_df.columns = ['Fact Type', 'Data']
+    
+    mars_facts_html = mars_facts_df.to_html(header=True, index=False)
+    
+    return mars_facts_html
+
+
     
 # Scrape Mars Hemisphere Images
 def marsHemisphereImages():    
+    browser = init_browser()
+    usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(usgs_url)
+    time.sleep(5)
+        
+    label1 = browser.find_by_tag('h3')[0].text
+    label2 = browser.find_by_tag('h3')[1].text
+    label3 = browser.find_by_tag('h3')[2].text
+    label4 = browser.find_by_tag('h3')[3].text
+    time.sleep(5)
+
+    browser.find_by_css('.thumb')[0].click()
+    image1 = browser.find_by_text('Sample')['href']
+    browser.back()
+    time.sleep(5)
+
+    browser.find_by_css('.thumb')[1].click()
+    image2 = browser.find_by_text('Sample')['href']
+    browser.back()
+    time.sleep(5)
+
+    browser.find_by_css('.thumb')[2].click()
+    image3 = browser.find_by_text('Sample')['href']
+    browser.back()
+    time.sleep(5)
+
+    browser.find_by_css('.thumb')[3].click()
+    image4 = browser.find_by_text('Sample')['href']
+    browser.back()
+    time.sleep(5)
+
+    mars_hem_images=[{'title':label1, 'image_link': image1},
+                   {'title':label2,'image_link': image2},
+                   {'title':label3, 'image_link': image3},
+                   {'title':label4,'image_link':image4}]
+
+    return mars_hem_images
+
+    browser.quit()
+
+   
+    
+   
+    
+    
     
